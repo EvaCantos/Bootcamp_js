8748186ec69d573f692058abd43c68bf77c43dfe
Eva Cantos <cantos.eva@gmail.com>
2023-05-18 21:45:04 +0200

    Correcciones

--- Modulo_01/js/demo.js
+++ Modulo_01/js/demo.js
@@ -1,4 +1,4 @@
-document.getElementById ("name").value= "Eva"
-document.getElementById ("surname").value= "Cantos"
+document.getElementById ("name").value= "Eva";
+document.getElementById ("surname").value= "Cantos";
 
-document.getElementById ("Avatar")
+document.getElementById ("Avatar"). src= "img/usuario-sin-foto.png";
